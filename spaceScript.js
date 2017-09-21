901c813a322155433cb0d00bd53d2f28e544545b
McLeodN <31251637+mcleod805@users.noreply.github.com>
2017-09-21 10:18:21 -0700

    Update spaceScript.js

--- spaceScript.js
+++ spaceScript.js
@@ -3,3 +3,47 @@ $(document).ready(function() {
     $("#sunimg").toggle();
 	});
 });
+$(document).ready(function() {
+	$("#mercury").click(function() {
+    $("#mercuryimg").toggle();
+	});
+});
+$(document).ready(function() {
+	$("#venus").click(function() {
+    $("#venusimg").toggle();
+	});
+});
+$(document).ready(function() {
+	$("#earth").click(function() {
+    $("#earthimg").toggle();
+	});
+});
+$(document).ready(function() {
+	$("#mars").click(function() {
+    $("#marsimg").toggle();
+	});
+});
+$(document).ready(function() {
+	$("#jupiter").click(function() {
+    $("#jupiterimg").toggle();
+	});
+});
+$(document).ready(function() {
+	$("#saturn").click(function() {
+    $("#saturnimg").toggle();
+	});
+});
+$(document).ready(function() {
+	$("#unanus").click(function() {
+    $("#uranusimg").toggle();
+	});
+});
+$(document).ready(function() {
+	$("#neptune").click(function() {
+    $("#neptuneimg").toggle();
+	});
+});
+$(document).ready(function() {
+	$("#pluto").click(function() {
+    $("#plutoimg").toggle();
+	});
