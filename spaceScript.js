1e9a192ba16f1c05cf891c66a474854860e5a279
calliefausey <31317882+calliefausey@users.noreply.github.com>
2017-09-26 09:59:18 -0700

    Update spaceScript.js

--- spaceScript.js
+++ spaceScript.js
@@ -1,49 +1,30 @@
-$(document).ready(function() {
-	$("#sun").click(function() {
-    $("#sunimg").toggle();
-	});
-});
-$(document).ready(function() {
-	$("#mercury").click(function() {
-    $("#showMercury").toggle();
-	});
-});
-$(document).ready(function() {
-	$("#venus").click(function() {
-    $("#venusimg").toggle();
-	});
-});
-$(document).ready(function() {
-	$("#earth").click(function() {
-    $("#earthimg").toggle();
-	});
-});
-$(document).ready(function() {
-	$("#mars").click(function() {
-    $("#marsimg").toggle();
-	});
-});
-$(document).ready(function() {
-	$("#jupiter").click(function() {
-    $("#jupiterimg").toggle();
-	});
-});
-$(document).ready(function() {
-	$("#saturn").click(function() {
-    $("#saturnimg").toggle();
-	});
-});
-$(document).ready(function() {
-	$("#unanus").click(function() {
-    $("#uranusimg").toggle();
-	});
-});
-$(document).ready(function() {
-	$("#neptune").click(function() {
-    $("#neptuneimg").toggle();
-	});
-});
-$(document).ready(function() {
-	$("#pluto").click(function() {
-    $("#plutoimg").toggle();
-	});
+body {
+	text-align: center;
+	background-image: url("twinkle.jpg");
+}
+
+.header {
+	 font-family: Comic Sans Ms;
+	 text-align: center;
+	 color: rgb(106, 90, 205);
+	 font-size: 50px;
+}
+
+.YW {
+    color: rgb(106, 90, 205);
+	font-size: 15px;
+	
+}
+
+#sunimg {
+	display: none;
+}
+
+#showSun {
+	display: none;
+}
+
+p {
+	font-size: 25px;
+	color: yellow;
+}
